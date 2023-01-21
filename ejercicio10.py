791ee3ae4623c73f62ae7a08c33e33d4ff6566f5
Patricio Muñoz <pmherras@gmail.com>
2023-01-21 20:02:21 -0300

    first commit

---
+++ ejercicio10.py
@@ -0,0 +1,7 @@
+print("dame un valor 1")
+a=input()
+print("dame un valor 2")
+b=input()
+
+c=int(a)+int(b)
+print("la suma de los varlores es;",c)
---
+++ ejercicio12.py
@@ -0,0 +1,34 @@
+#probando las operaciones aritmneticas
+
+print("dame un valor 1")
+a=int(input())
+
+print("dame un valor 2")
+b=int(input())
+
+#ejemplo  suma
+suma=a+b
+
+#ejemplo resta
+resta=a-b
+
+#ejemplo multiplicacion
+multiplicacion=a*b
+
+#ejemplo division
+division=a/b
+
+#exponente
+exponente = a**b
+
+#floor
+floor=a//b
+
+#residuo
+modulo= a%b  
+
+print(suma)
+print(resta)
+print(multiplicacion)
+print(division)
+print(modulo)   
